a83aee0685ebc102065fdbb3aa3ba07133dfdace
max <raiyin@ya.ru>
2019-10-28 23:31:00 +0300

    Made first variant of the web-part

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ from pathlib import Path
 from miscellanea import ConfigManager
 from miscellanea import FileLogger, FakeTestLogger, BackupManager
 import os.path
+from werkzeug.debug import DebuggedApplication
+from flask import Flask
+import json
 
 app = Flask(__name__)
+app.run(debug=True)
 
 
 @app.route('/')
@@ -24,23 +28,29 @@ def index():
     db_manager = DbManager.DbManager(config_manager, logger)
 
     # Запрос к БД.
-    reports = db_manager.get_reports()
+    reports_db = db_manager.get_reports()
 
     # Преобразование полученных данных в массив словарей.
-    reports_dict = []
-    for report in reports:
-        reports_dict.append(
-            {'name': report[1]},
-            {'decription': report[2]},
-            {'columns': report[3]},
-            {'x_title': report[4]},
-            {'y_title': report[5]},
-            {'body': report[6]}
-        )
+    reports = []
+    i = 0
+    for report in reports_db:
+        reports.append(list(report))
+        reports[i][3] = json.loads(report[3])
+        i = i + 1
+        # reports_dict.append(
+        #    {'name': reports[1]},
+        #    {'decription': reports[2]},
+        #    {'columns': reports[3]},
+        #    {'x_title': reports[4]},
+        #    {'y_title': reports[5]},
+        #    {'body': reports[6]}
+        # )
 
     # Передача в шаблон и его вызов.
-    return render_template("index.html", reports=reports)
+    # return render_template("index.html", reports=reports_dict)
+    user = {'username': 'Miguel'}
+    return render_template('index.html', title='Home', user=user, reports=reports)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
